0ce92458d322deaf86d2a01856e640cc3e9b2e0c
thebadcc <95499220+thebadcc-eth@users.noreply.github.com>
2022-06-27 21:44:35 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
-import mergeImages from 'merge-images';
- 
+window.onload = function() {
 mergeImages(['/background/blue.png', '/type/ape mfer.png'])
   .then(b64 => document.querySelector('img').src = b64);
+};
+
