d4cb611234afc1a9e7adb9f766333dec3a7f5854
thebadcc <95499220+thebadcc-eth@users.noreply.github.com>
2022-07-01 13:49:02 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,240 @@
-window.onload = function() {
-  import mergeImages from 'merge-images';
-mergeImages(['/background/blue.png', '/type/ape mfer.png'])
-  .then(b64 => document.querySelector('img').src = b64);
-};
+var baseURL = 'https://www.cerealbox.wtf';
 
+const background = [];
+background[0]= "/traits/background/background-blue.png";
+background[1]= "/traits/background/background-yellow.png";
+background[2]= "/traits/background/background-green.png";
+background[3]= "/traits/background/background-orange.png";
+background[4]= "/traits/background/background-red.png";
+background[5]= "/traits/background/background-space.png";
+background[6]= "/traits/background/background-tree.png";
+
+const type = [];
+type[0]= "/traits/type/type-plainmfer.png";
+type[1]= "/traits/type/type-charcoalmfer.png";
+type[2]= "/traits/type/type-zombiemfer.png";
+type[3]= "/traits/type/type-apemfer.png";
+type[4]= "/traits/type/type-alienmfer.png";
+
+
+const watch420 = [];
+watch420[0]= "/traits/watch/watch-argoblack.png";
+watch420[1]= "/traits/watch/watch-argowhite.png";
+watch420[2]= "/traits/watch/watch-oystergold.png";
+watch420[3]= "/traits/watch/watch-oystersilver.png";
+watch420[4]= "/traits/watch/watch-subbat.png";
+watch420[5]= "/traits/watch/watch-subblack.png";
+watch420[6]= "/traits/watch/watch-subblue.png";
+watch420[7]= "/traits/watch/watch-subcola.png";
+watch420[8]= "/traits/watch/watch-sublantern.png";
+watch420[9]= "/traits/watch/watch-subred.png";
+watch420[10]= "/traits/watch/watch-subrose.png";
+watch420[11]= "/traits/watch/watch-subturquoise.png";
+
+const beard = [];
+beard[0]= "/traits/beard/beard-fullbeard.png";
+beard[1]= "/traits/beard/beard-shadowbeard.png";
+
+const eyes = [];
+eyes[0]= "/traits/eyes/eyes-3dglasses.png";
+eyes[1]= "/traits/eyes/eyes-alieneyes.png";
+eyes[2]= "/traits/eyes/eyes-eyemask.png";
+eyes[3]= "/traits/eyes/eyes-eyepatch.png";
+eyes[4]= "/traits/eyes/eyes-nerdglasses.png";
+eyes[5]= "/traits/eyes/eyes-purpleshades.png";
+eyes[6]= "/traits/eyes/eyes-regulareyes.png";
+eyes[7]= "/traits/eyes/eyes-shades.png";
+eyes[8]= "/traits/eyes/eyes-vr.png";
+eyes[9]= "/traits/eyes/eyes-zombieeyes.png";
+
+const hoodies = [];
+hoodies[0]= "/traits/hatoverheadphones/hatoverheadphones-hoodie.png";
+
+const headphones = [];
+headphones[0]= "/traits/headphones/headphones-rcsheadphones.png";
+headphones[1]= "/traits/headphones/headphones-blackheadphones.png";
+headphones[2]= "/traits/headphones/headphones-blueheadphones.png";
+headphones[3]= "/traits/headphones/headphones-goldheadphones.png";
+headphones[4]= "/traits/headphones/headphones-greenheadphones.png";
+headphones[5]= "/traits/headphones/headphones-linedheadphones.png";
+headphones[6]= "/traits/headphones/headphones-pinkheadphones.png";
+headphones[7]= "/traits/headphones/headphones-redheadphones.png";
+headphones[8]= "/traits/headphones/headphones-whiteheadphones.png";
+
+
+const hatsUnder = [];
+hatsUnder[0]= "/traits/hatunderheadphones/hatunderheadphones-bandanablue.png";
+hatsUnder[1]= "/traits/hatunderheadphones/hatunderheadphones-bandanadarkgray.png";
+hatsUnder[2]= "/traits/hatunderheadphones/hatunderheadphones-bandanared.png";
+hatsUnder[3]= "/traits/hatunderheadphones/hatunderheadphones-beaniemonochrome.png";
+hatsUnder[4]= "/traits/hatunderheadphones/hatunderheadphones-beanie.png";
+hatsUnder[5]= "/traits/hatunderheadphones/hatunderheadphones-capmonochrome.png";
+hatsUnder[6]= "/traits/hatunderheadphones/hatunderheadphones-cappurple.png";
+hatsUnder[7]= "/traits/hatunderheadphones/hatunderheadphones-headbandbluegreen.png";
+hatsUnder[8]= "/traits/hatunderheadphones/hatunderheadphones-headbandbluered.png";
+hatsUnder[9]= "/traits/hatunderheadphones/hatunderheadphones-headbandbluewhite.png";
+hatsUnder[10]= "/traits/hatunderheadphones/hatunderheadphones-headbandgreenwhite.png";
+hatsUnder[11]= "/traits/hatunderheadphones/hatunderheadphones-headbandpinkwhite.png";
+hatsUnder[12]= "/traits/hatunderheadphones/hatunderheadphones-knitatlanta.png";
+hatsUnder[13]= "/traits/hatunderheadphones/hatunderheadphones-knitbaltimore.png";
+hatsUnder[14]= "/traits/hatunderheadphones/hatunderheadphones-knitbuffalo.png";
+hatsUnder[15]= "/traits/hatunderheadphones/hatunderheadphones-knitchicago.png";
+hatsUnder[16]= "/traits/hatunderheadphones/hatunderheadphones-knitcleveland.png";
+hatsUnder[17]= "/traits/hatunderheadphones/hatunderheadphones-knitdallas.png";
+hatsUnder[18]= "/traits/hatunderheadphones/hatunderheadphones-knitkc.png";
+hatsUnder[19]= "/traits/hatunderheadphones/hatunderheadphones-knitlasvegas.png";
+hatsUnder[20]= "/traits/hatunderheadphones/hatunderheadphones-knitmiami.png";
+hatsUnder[21]= "/traits/hatunderheadphones/hatunderheadphones-knitnewyork.png";
+hatsUnder[22]= "/traits/hatunderheadphones/hatunderheadphones-knitpittsburgh.png";
+hatsUnder[23]= "/traits/hatunderheadphones/hatunderheadphones-knitsanfran.png";
+
+const hatsOver = [];
+hatsOver[0]= "/traits/hatoverheadphones/hatoverheadphones-cowboyhat.png";
+hatsOver[1]= "/traits/hatoverheadphones/hatoverheadphones-pilothelmet.png";
+hatsOver[2]= "/traits/hatoverheadphones/hatoverheadphones-tophat.png";
+
+const shortHair = [];
+shortHair[0]= "/traits/shorthair/shorthair-messyblack.png";
+shortHair[1]= "/traits/shorthair/shorthair-messypurple.png";
+shortHair[2]= "/traits/shorthair/shorthair-messyred.png";
+shortHair[3]= "/traits/shorthair/shorthair-messyyellow.png";
+shortHair[4]= "/traits/shorthair/shorthair-mohawkblack.png";
+shortHair[5]= "/traits/shorthair/shorthair-mohawkblue.png";
+shortHair[6]= "/traits/shorthair/shorthair-mohawkgreen.png";
+shortHair[7]= "/traits/shorthair/shorthair-mohawkpink.png";
+shortHair[8]= "/traits/shorthair/shorthair-mohawkpurple.png";
+shortHair[9]= "/traits/shorthair/shorthair-mohawkred.png";
+shortHair[10]= "/traits/shorthair/shorthair-mohawkyellow.png";
+
+const longHair = [];
+longHair[0]= "/traits/longhair/longhair-longhairblack.png";
+longHair[1]= "/traits/longhair/longhair-longhairyellow.png";
+
+const mouth = [];
+mouth[0]= "/traits/mouth/mouth-flat.png";
+mouth[1]= "/traits/mouth/mouth-smile.png";
+
+const shirt = [];
+shirt[0]= "/traits/shirt/shirt-collaredshirtblue.png";
+shirt[1]= "/traits/shirt/shirt-collaredshirtgreen.png";
+shirt[2]= "/traits/shirt/shirt-collaredshirtpink.png";
+shirt[3]= "/traits/shirt/shirt-collaredshirtblue.png";
+shirt[4]= "/traits/shirt/shirt-collaredshirtgreen.png";
+shirt[5]= "/traits/shirt/shirt-collaredshirtpink.png";
+shirt[6]= "/traits/shirt/shirt-collaredshirtblue.png";
+shirt[7]= "/traits/shirt/shirt-collaredshirtgreen.png";
+shirt[8]= "/traits/shirt/shirt-collaredshirtpink.png";
+shirt[9]= "/traits/chain/chain-goldchain.png";
+shirt[10]= "/traits/chain/chain-silverchain.png";
+
+const smoke = [];
+smoke[0]= "/traits/smoke/smoke-cigblack.png";
+smoke[1]= "/traits/smoke/smoke-cigwhite.png";
+smoke[2]= "/traits/smoke/smoke-pipe.png";
+
+function generateMfers(mfers) {
+
+document.getElementsByClassName("html-content-holder")[0].style.display = "inherit";
+ const elements = document.getElementsByClassName('mferImg');
+ 
+var div = document.createElement("div");
+div.setAttribute('class', 'mferImg');
+document.getElementsByClassName('mferWrapper')[0].appendChild(div);
+elements[0].parentNode.removeChild(elements[0]);
+
+for (let i = 0; i < mfers; i++){
+
+var _background = "url(" + baseURL + background[Math.floor(Math.random() * 7)] + ")";
+var _type = "url(" + baseURL + type[Math.floor(Math.random() * 5)] + "), ";
+var _eyes = "url(" + baseURL + eyes[Math.floor(Math.random() * 10)] + "), ";
+var _mouth = "url(" + baseURL + mouth[Math.floor(Math.random() * 2)] + "), ";
+var _headphones = "url(" + baseURL + headphones[Math.floor(Math.random() * 9)] + "), ";
+var rollSmoke = Math.floor(Math.random() * 10);
+if (rollSmoke <= 7) {
+		var _smoke = "url(" + baseURL + smoke[Math.floor(Math.random() * 3)] + "), ";
+} else {
+		var _smoke = ""
+}
+var rollWatch = Math.floor(Math.random() * 10);
+if (rollWatch <= 5) {
+		var _watch420 = "url(" + baseURL + watch420[Math.floor(Math.random() * 12)] + "), ";
+} else {
+		var _watch420 = ""
+}
+var rollBeard = Math.floor(Math.random() * 10);
+if (rollBeard <= 3) {
+		var _beard = "url(" + baseURL + beard[Math.floor(Math.random() * 2)] + "), ";
+} else {
+		var _beard = "";
+}
+var rollHoodies = Math.floor(Math.random() * 10);
+if (rollHoodies <= 1) {
+		var _hoodies = "url(" + baseURL + hoodies[0] + "), ";
+    var seed = _hoodies + _smoke + _watch420 + _mouth + _headphones + _eyes + _beard + _type + _background;
+    document.getElementsByClassName("html-content-holder")[0].style.backgroundImage = seed;
+    
+} else {
+		
+    var rollShirts = Math.floor(Math.random() * 10);
+    if (rollShirts <= 5) {
+    var _shirt = "url(" + baseURL + shirt[Math.floor(Math.random() * 11)] + "), ";
+    } else {
+    var _shirt = "";
+    }
+    
+    var rollOverUnder = Math.floor(Math.random() * 10);
+   
+    if (rollOverUnder <= 2) {
+      var _hatsUnder = "url(" + baseURL + hatsUnder[Math.floor(Math.random() * 24)] + "), ";
+      var rollLongHair = Math.floor(Math.random() * 10);
+      if (rollLongHair <= 3) {
+				var _longHair = "url(" + baseURL + longHair[Math.floor(Math.random() * 2)] + "), ";
+      } else {
+				var _longHair = ""
+      }
+    	    var seed = _smoke + _watch420 + _mouth + _headphones  + _eyes + _hatsUnder  + _longHair + _shirt + _beard + _type + _background;
+    document.getElementsByClassName("html-content-holder")[0].style.backgroundImage = seed;
+    
+    } else if (rollOverUnder >= 2 && rollOverUnder < 4){
+      var _hatsOver = "url(" + baseURL + hatsOver[Math.floor(Math.random() * 3)] + "), ";
+      var rollLongHair = Math.floor(Math.random() * 10);
+      if (rollLongHair <= 4) {
+				var _longHair = "url(" + baseURL + longHair[Math.floor(Math.random() * 2)] + "), ";
+      } else {
+				var _longHair = ""
+      }
+    	    var seed = _smoke  + _watch420 + _mouth + _hatsOver + _headphones + _eyes  + _longHair + _shirt + _beard + _type + _background;
+    document.getElementsByClassName("html-content-holder")[0].style.backgroundImage = seed;
+
+
+    } else if (rollOverUnder >= 4 && rollOverUnder < 6){
+       
+       var rollLongHair = Math.floor(Math.random() * 10);
+      if (rollLongHair <= 4) {
+				var _longHair = "url(" + baseURL + longHair[Math.floor(Math.random() * 2)] + "), ";
+                       var seed = _smoke + _watch420 + _mouth + _headphones + _longHair + _eyes + _shirt + _beard + _type + _background;
+    document.getElementsByClassName("html-content-holder")[0].style.backgroundImage = seed;
+      } else {
+				var _shortHair = "url(" + baseURL + shortHair[Math.floor(Math.random() * 12)] + "), ";
+               var seed = _smoke + _watch420 + _mouth + _headphones + _shortHair + _eyes + _shirt + _beard + _type + _background;
+    document.getElementsByClassName("html-content-holder")[0].style.backgroundImage = seed;
+      }
+       
+    } else if (rollOverUnder >= 6) {
+            var seed = _smoke + _watch420 + _mouth + _headphones + _eyes + _shirt + _beard + _type + _background;
+    document.getElementsByClassName("html-content-holder")[0].style.backgroundImage = seed;
+    }
+    
+}
+
+html2canvas(document.getElementsByClassName("html-content-holder")[0],{
+	allowTaint: true,
+	useCORS: true
+	}).then(function (canvas) {
+
+    document.getElementsByClassName("mferImg")[0].appendChild(canvas);
+	});
+}
+
+document.getElementsByClassName("html-content-holder")[0].style.display = "none";
+}
