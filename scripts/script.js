8d9a92eaa65c97dcd99ada7227b502851611931f
thebadcc <95499220+thebadcc-eth@users.noreply.github.com>
2022-07-06 21:59:14 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -382,7 +382,7 @@ document.getElementsByClassName("html-content-holder")[0].style.display = "none"
 
 function buildMfers() {
 var quant = document.getElementsByClassName("quantity")[0].value;
-var _seed = document.getElementsByClassName("seed")[0].innerHTML;
+var _seed = document.getElementsByClassName("seed")[0].value;
 generateMfers(quant, _seed);
 
 }
