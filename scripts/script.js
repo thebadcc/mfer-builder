389c790883b5037392387b138a27ff12dd031663
thebadcc <95499220+thebadcc-eth@users.noreply.github.com>
2022-07-12 21:20:13 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -368,8 +368,7 @@ function generateMfers(mfers, seedinput) {
 	var pngUrl = canvas.toDataURL("image/png");
 	var mferImg = document.createElement("img");
 	mferImg.src = pngUrl;
-	mferImg.className = 'mfers';
-	mferImg.alt = seed; 	
+	mferImg.className = 'mfers';	
 	document.getElementsByClassName("mferImg")[0].appendChild(mferImg);
 	});
 }
