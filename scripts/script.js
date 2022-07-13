6d609f03bebbb29b5e008f85b479f8774b4e57d1
thebadcc <95499220+thebadcc-eth@users.noreply.github.com>
2022-07-12 21:43:24 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -425,7 +425,8 @@ function wtf() {
 	filters[4]= "contrast(5)";
 	filters[5]= "hue-rotate(3.142rad)";
 	var count = -1;
+	var selectFilter = filters[++count % filters.length];
 	for (let i = 0; i < document.getElementsByClassName("mfers").length; i++){
-		document.getElementsByClassName("mfers")[i].style.filter = filters[++count % filters.length];
+		document.getElementsByClassName("mfers")[i].style.filter = selectFilter;
 	};
 }
