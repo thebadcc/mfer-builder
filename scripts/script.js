3ca1f2122d0649e44bd531a5201cebef3fce5429
thebadcc <95499220+thebadcc-eth@users.noreply.github.com>
2022-07-15 17:58:45 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -366,7 +366,7 @@ function generateMfers(mfers, seedinput) {
 			var _longHair = "";
     	}
 	}
-	var seed = _hoodies + _smoke + _watch420 + _mouth + _hatsOver + _headphones + _hatsUnder + _longHair + _shortHair + _shirt + _eyes + _beard + _type + _background;
+	var seed = _hoodies + _smoke + _watch420 + _hatsOver + _headphones + _hatsUnder + _longHair + _shortHair + _shirt + _eyes + _mouth + _beard + _type + _background;
 	document.getElementsByClassName("html-content-holder")[0].style.backgroundImage = seed;
 	window.scrollTo(0,0);	
 	html2canvas(document.getElementsByClassName("html-content-holder")[0],{allowTaint: true,useCORS: true}).then(function (canvas) {
