5a44e2dfcfbb86a09c747cb439dade55ed416571
thebadcc <95499220+thebadcc-eth@users.noreply.github.com>
2022-07-16 23:43:43 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -371,8 +371,17 @@ function generateMfers(mfers, seedinput) {
 	}
 	window.scrollTo(0,0);	
 	var seed = _hoodies + _smoke + _watch420 + _hatsOver + _headphones + _hatsUnder + _longHair + _shortHair + _shirt + _eyes + _mouth + _beard + _type + _background;
-	console.log(seed);
+	if (seed = '') {
+		console.log('true');
+	}
 	document.getElementsByClassName("html-content-holder")[i].style.backgroundImage = seed;
+	html2canvas(document.getElementsByClassName("html-content-holder")[i],{allowTaint: true,useCORS: true}).then(function (canvas) {
+		var pngUrl = canvas.toDataURL("image/png");
+		var mferImg = document.createElement("img");
+		mferImg.src = pngUrl;
+		mferImg.className = 'mfers';
+		document.getElementsByClassName("mferImg")[0].appendChild(mferImg);
+	});
 	document.getElementsByClassName("html-content-holder")[i].style.display = "none";	
 }
 
