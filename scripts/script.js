ad368be095b31ac870a96dbe9082d1b9cb2a2cd3
thebadcc <95499220+thebadcc@users.noreply.github.com>
2022-09-14 12:47:25 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -490,3 +490,12 @@ function wtf() {
 	}
 	
 }
+
+function dropdown() {
+  var x = document.getElementsByClassName("dropdown")[0];
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
