41e92ad975b2d1eec91df99454c04d2fcea2089b
thebadcc <95499220+thebadcc@users.noreply.github.com>
2022-09-21 07:28:10 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,6 @@ projects[2] = "eos";
 projects[3] = "mfersAhead";
 projects[4] = "mfersBehind";
 projects[5] = "sketchyMfers";
-projects[6] = "mferDinos";
 
 const background = [];
 background[0]= "/background/blue.png";
@@ -202,7 +201,7 @@ function generateMfers(mfers, seedinput) {
 		var shortHairRNG = shortHair[Math.floor(Math.random() * 12)];
 		
 		if (baseID == "random") {
-			var base = projects[Math.floor(Math.random() * 7)];
+			var base = projects[Math.floor(Math.random() * 6)];
 		} else {
 			var base = document.getElementsByClassName('base')[0].value;
 		}
@@ -426,7 +425,7 @@ function generateMfers(mfers, seedinput) {
 	}
 	
 	window.scrollTo(0,0);	
-	if (base == "mfers" || base == "creyzies" || base == "eos" || base == "mferDinos") {
+	if (base == "mfers" || base == "creyzies" || base == "eos") {
 	var seed = _hoodies + _watch420 + _smoke + _hatsOver + _headphones + _hatsUnder + _longHair + _shortHair + _shirt + _eyes + _mouth + _beard + _type + _background;
 	} else if (base == "mfersBehind") {
 	var seed = _hoodies + _watch420  + _hatsOver + _shortHair + _headphones + _hatsUnder + _longHair + _smoke + _shirt + _eyes + _mouth + _beard + _type + _background;
