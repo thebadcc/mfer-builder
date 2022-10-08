9b4a3b0cb709db0a94b5a84c281f9a1a06bba7bf
thebadcc <95499220+thebadcc@users.noreply.github.com>
2022-10-08 00:37:22 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -514,8 +514,6 @@ function generateMfers(count, seedinput) {
 		mferImg.className = 'mfers';
 		document.getElementsByClassName("mferImg")[0].appendChild(mferImg);
 	});
-	document.getElementsByClassName("html-content-holder")[0].style.backgroundImage = 'URL(https://raw.githubusercontent.com/thebadcc-eth/mfer-builder/main/images/eos/endofsartoshi.jpg)';
-	
 }
 
 	document.getElementsByClassName("html-content-holder")[0].style.display = "none";
